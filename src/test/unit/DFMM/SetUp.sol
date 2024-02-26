50134308855e1d0f4d4e19a2a898fbd4648617af
clemlak <clementlakhal@gmail.com>
2024-02-26 19:54:22 +0400

    test: add token X and Y to DFMM setup

--- src/test/unit/DFMM/SetUp.sol
+++ src/test/unit/DFMM/SetUp.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
 import "src/test/helpers/SetUp.sol";
 import "src/test/helpers/MockStrategy.sol";
 
 contract DFMMSetUp is SetUp {
     MockStrategy strategy;
+    MockERC20 tokenX;
+    MockERC20 tokenY;
+
     uint256 public POOL_ID;
 
     function setUp() public override {
         SetUp.setUp();
         strategy = new MockStrategy(address(dfmm));
+        tokenX = new MockERC20("tokenX", "X", 18);
+        tokenY = new MockERC20("tokenY", "Y", 18);
     }
 
     modifier init() {
