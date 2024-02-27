67a3cb0558fa375af304c679431166ea3e12ab29
clemlak <clementlakhal@gmail.com>
2024-02-27 09:53:25 +0400

    test: update DFMM general setup

--- src/test/helpers/SetUp.sol
+++ src/test/helpers/SetUp.sol
@@ -2,16 +2,18 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "solmate/test/utils/mocks/MockERC20.sol";
-import "../../DFMM.sol";
-import "../helpers/Lex.sol";
-import "../helpers/MockStrategy.sol";
+import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
+import { DFMM } from "src/DFMM.sol";
+import { IDFMM } from "src/interfaces/IDFMM.sol";
+import { LPToken } from "src/LPToken.sol";
+import { Lex } from "src/test/helpers/Lex.sol";
 
 contract SetUp is Test {
     DFMM dfmm;
     Lex lex;
     MockERC20 tokenX;
     MockERC20 tokenY;
+    LPToken lpTokenImplementation;
 
     uint256 public constant TEST_SWAP_FEE = 0.003 ether;
 
@@ -22,7 +24,8 @@ contract SetUp is Test {
         tokenY.mint(address(this), 100e18);
 
         lex = new Lex(address(tokenX), address(tokenY), 1 ether);
-        dfmm = new DFMM(address(0));
+        LPToken lpToken = new LPToken();
+        dfmm = new DFMM(address(lpToken));
 
         tokenX.approve(address(dfmm), type(uint256).max);
         tokenY.approve(address(dfmm), type(uint256).max);
