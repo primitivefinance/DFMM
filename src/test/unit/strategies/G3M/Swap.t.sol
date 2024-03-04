b66b0b58463dabba342368c5f05d95986bc9de4e
clemlak <clementlakhal@gmail.com>
2024-03-04 15:02:10 +0400

    test: update and fix G3MSwapTest tests

--- src/test/unit/strategies/G3M/Swap.t.sol
+++ src/test/unit/strategies/G3M/Swap.t.sol
@@ -9,55 +9,44 @@ contract G3MSwapTest is G3MSetUp {
     using FixedPointMathLib for int256;
 
     function test_G3M_swap_SwapsXforY() public init {
-        uint256 amountIn = 0.1 ether;
-        bool swapXForY = true;
-
         uint256 preDfmmBalanceX = tokenX.balanceOf(address(dfmm));
         uint256 preDfmmBalanceY = tokenY.balanceOf(address(dfmm));
         uint256 preUserBalanceX = tokenX.balanceOf(address(this));
         uint256 preUserBalanceY = tokenY.balanceOf(address(this));
 
-        (bool valid, uint256 amountOut,, bytes memory payload) =
+        uint256 amountIn = 0.1 ether;
+        bool swapXForY = true;
+
+        (, uint256 amountOut,, bytes memory payload) =
             solver.simulateSwap(POOL_ID, swapXForY, amountIn);
-        assertEq(valid, true);
-        (uint256 inputAmount, uint256 outputAmount) =
-            dfmm.swap(POOL_ID, payload);
+        (uint256 deltaX, uint256 deltaY) = dfmm.swap(POOL_ID, payload);
+        assertEq(amountIn, deltaX);
+        assertEq(amountOut, deltaY);
 
-        assertEq(tokenX.balanceOf(address(dfmm)), preDfmmBalanceX + inputAmount);
-        assertEq(
-            tokenY.balanceOf(address(dfmm)), preDfmmBalanceY - outputAmount
-        );
-
-        assertEq(tokenX.balanceOf(address(this)), preUserBalanceX - inputAmount);
-        assertEq(
-            tokenY.balanceOf(address(this)), preUserBalanceY + outputAmount
-        );
+        assertEq(tokenX.balanceOf(address(dfmm)), preDfmmBalanceX + deltaX);
+        assertEq(tokenY.balanceOf(address(dfmm)), preDfmmBalanceY - deltaY);
+        assertEq(tokenX.balanceOf(address(this)), preUserBalanceX - deltaX);
+        assertEq(tokenY.balanceOf(address(this)), preUserBalanceY + deltaY);
     }
 
     function test_G3M_swap_SwapsYforX() public init {
         uint256 preDfmmBalanceX = tokenX.balanceOf(address(dfmm));
         uint256 preDfmmBalanceY = tokenY.balanceOf(address(dfmm));
-
         uint256 preUserBalanceX = tokenX.balanceOf(address(this));
         uint256 preUserBalanceY = tokenY.balanceOf(address(this));
 
         uint256 amountIn = 0.1 ether;
         bool swapXForY = false;
 
-        (bool valid,,, bytes memory payload) =
+        (, uint256 amountOut,, bytes memory payload) =
             solver.simulateSwap(POOL_ID, swapXForY, amountIn);
-        assertEq(valid, true);
-        (uint256 inputAmount, uint256 outputAmount) =
-            dfmm.swap(POOL_ID, payload);
+        (uint256 deltaX, uint256 deltaY) = dfmm.swap(POOL_ID, payload);
+        assertEq(amountIn, deltaY);
+        assertEq(amountOut, deltaX);
 
-        assertEq(tokenY.balanceOf(address(dfmm)), preDfmmBalanceX + inputAmount);
-        assertEq(
-            tokenX.balanceOf(address(dfmm)), preDfmmBalanceY - outputAmount
-        );
-
-        assertEq(tokenY.balanceOf(address(this)), preUserBalanceX - inputAmount);
-        assertEq(
-            tokenX.balanceOf(address(this)), preUserBalanceY + outputAmount
-        );
+        assertEq(tokenX.balanceOf(address(dfmm)), preDfmmBalanceX - deltaX);
+        assertEq(tokenY.balanceOf(address(dfmm)), preDfmmBalanceY + deltaY);
+        assertEq(tokenX.balanceOf(address(this)), preUserBalanceX + deltaX);
+        assertEq(tokenY.balanceOf(address(this)), preUserBalanceY - deltaY);
     }
 }
