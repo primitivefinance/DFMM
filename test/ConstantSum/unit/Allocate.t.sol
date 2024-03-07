86aa4502365bb51112cd162f3027caf8a0d49b97
clemlak <clementlakhal@gmail.com>
2024-03-07 18:53:06 +0400

    test: add ConstantSum allocate test

--- test/ConstantSum/unit/Allocate.t.sol
+++ test/ConstantSum/unit/Allocate.t.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { console } from "forge-std/console.sol";
 import { ConstantSumSetUp } from "./SetUp.sol";
 
-contract ConstantSumAllocateTest is ConstantSumSetUp { }
+contract ConstantSumAllocateTest is ConstantSumSetUp {
+    // TODO: This is the perfect example of a bad test, please
+    // improve this.
+    function test_ConstantSum_allocate_Works() public defaultPool {
+        uint256 deltaX = 0.1 ether;
+        uint256 deltaY = 0.1 ether;
+        (bool valid, bytes memory data) =
+            solver.simulateAllocate(POOL_ID, deltaX, deltaY);
+        console.log("valid", valid);
+    }
+}
