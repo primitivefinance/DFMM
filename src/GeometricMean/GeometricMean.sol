b1df2243509f36c9ef7c10bc07f70a9cacba748e
clemlak <clementlakhal@gmail.com>
2024-03-07 13:33:12 +0400

    feat: implement Strategy abstract contract in GeometricMean strategy

--- src/GeometricMean/GeometricMean.sol
+++ src/GeometricMean/GeometricMean.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.13;
 
-import "./GeometricMeanLib.sol";
-import "src/interfaces/IStrategy.sol";
-import "src/lib/DynamicParamLib.sol";
-import "./G3MExtendedLib.sol";
+import { Strategy, IStrategy } from "src/Strategy.sol";
+import { DynamicParam, DynamicParamLib } from "src/lib/DynamicParamLib.sol";
+import { IDFMM } from "src/interfaces/IDFMM.sol";
+import { GeometricMeanLib, FixedPointMathLib } from "./GeometricMeanLib.sol";
+import { ONE } from "src/lib/StrategyLib.sol";
 
 /// @dev Parameterization of the GeometricMean curve.
 struct GeometricMeanParams {
@@ -17,7 +18,7 @@ struct GeometricMeanParams {
 /**
  * @notice Geometric Mean Market Maker.
  */
-contract GeometricMean is IStrategy {
+contract GeometricMean is Strategy {
     using FixedPointMathLib for uint256;
     using FixedPointMathLib for int256;
     using DynamicParamLib for DynamicParam;
@@ -29,26 +30,14 @@ contract GeometricMean is IStrategy {
     }
 
     /// @inheritdoc IStrategy
-    address public immutable dfmm;
-
-    /// @inheritdoc IStrategy
-    string public constant name = "GeometricMean";
+    string public constant override name = "GeometricMean";
 
     mapping(uint256 => InternalParams) public internalParams;
 
     /// @param dfmm_ Address of the DFMM contract.
-    constructor(address dfmm_) {
-        dfmm = dfmm_;
-    }
+    constructor(address dfmm_) Strategy(dfmm_) { }
 
-    // TODO: Move these errors into an interface
     error InvalidWeightX();
-
-    /// @dev Restricts the caller to the DFMM contract.
-    modifier onlyDFMM() {
-        if (msg.sender != address(dfmm)) revert NotDFMM();
-        _;
-    }
 
     /// @inheritdoc IStrategy
     function init(
@@ -109,152 +98,6 @@ contract GeometricMean is IStrategy {
         valid = -(EPSILON) < invariant && invariant < EPSILON;
     }
 
-    error DeltaError(uint256 expected, uint256 actual);
-
-    /// @inheritdoc IStrategy
-    function validateAllocate(
-        address,
-        uint256 poolId,
-        IDFMM.Pool calldata pool,
-        bytes calldata data
-    )
-        external
-        view
-        returns (
-            bool valid,
-            int256 invariant,
-            uint256 deltaX,
-            uint256 deltaY,
-            uint256 deltaLiquidity
-        )
-    {
-        (uint256 maxDeltaX, uint256 maxDeltaY, uint256 deltaL) =
-            abi.decode(data, (uint256, uint256, uint256));
-
-        // TODO: This is a small trick because `deltaLiquidity` cannot be used
-        // directly, let's fix this later.
-        deltaLiquidity = deltaL;
-
-        deltaX =
-            computeDeltaXGivenDeltaL(deltaL, pool.totalLiquidity, pool.reserveX);
-        deltaY = computeDeltaYGivenDeltaX(deltaX, pool.reserveX, pool.reserveY);
-
-        if (deltaX > maxDeltaX) {
-            revert DeltaError(maxDeltaX, deltaX);
-        }
-
-        if (deltaY > maxDeltaY) {
-            revert DeltaError(maxDeltaY, deltaY);
-        }
-
-        uint256 poolId = poolId;
-
-        invariant = GeometricMeanLib.tradingFunction(
-            pool.reserveX + deltaX,
-            pool.reserveY + deltaY,
-            pool.totalLiquidity + deltaLiquidity,
-            abi.decode(getPoolParams(poolId), (GeometricMeanParams))
-        );
-
-        valid = -(EPSILON) < invariant && invariant < EPSILON;
-    }
-
-    /// @inheritdoc IStrategy
-    function validateDeallocate(
-        address,
-        uint256 poolId,
-        IDFMM.Pool calldata pool,
-        bytes calldata data
-    )
-        external
-        view
-        returns (
-            bool valid,
-            int256 invariant,
-            uint256 deltaX,
-            uint256 deltaY,
-            uint256 deltaLiquidity
-        )
-    {
-        (uint256 minDeltaX, uint256 minDeltaY, uint256 deltaL) =
-            abi.decode(data, (uint256, uint256, uint256));
-        deltaLiquidity = deltaL;
-
-        deltaX =
-            computeDeltaXGivenDeltaL(deltaL, pool.totalLiquidity, pool.reserveX);
-        deltaY = computeDeltaYGivenDeltaX(deltaX, pool.reserveX, pool.reserveY);
-
-        if (minDeltaX > deltaX) {
-            revert DeltaError(minDeltaX, deltaX);
-        }
-
-        if (minDeltaY > deltaY) {
-            revert DeltaError(minDeltaY, deltaY);
-        }
-
-        uint256 poolId = poolId;
-
-        invariant = GeometricMeanLib.tradingFunction(
-            pool.reserveX - deltaX,
-            pool.reserveY - deltaY,
-            pool.totalLiquidity - deltaLiquidity,
-            abi.decode(getPoolParams(poolId), (GeometricMeanParams))
-        );
-
-        valid = -(EPSILON) < invariant && invariant < EPSILON;
-    }
-
-    /// @inheritdoc IStrategy
-    function validateSwap(
-        address,
-        uint256 poolId,
-        IDFMM.Pool calldata pool,
-        bytes memory data
-    )
-        external
-        view
-        returns (
-            bool valid,
-            int256 invariant,
-            uint256 deltaX,
-            uint256 deltaY,
-            uint256 deltaLiquidity,
-            bool isSwapXForY
-        )
-    {
-        GeometricMeanParams memory params =
-            abi.decode(getPoolParams(poolId), (GeometricMeanParams));
-
-        (deltaX, deltaY, isSwapXForY) =
-            abi.decode(data, (uint256, uint256, bool));
-
-        if (isSwapXForY) {
-            uint256 fees = deltaX.mulWadUp(params.swapFee);
-            deltaLiquidity = fees.mulWadUp(pool.totalLiquidity).divWadUp(
-                pool.reserveX
-            ).mulWadUp(params.wX);
-            invariant = GeometricMeanLib.tradingFunction(
-                pool.reserveX + deltaX,
-                pool.reserveY - deltaY,
-                pool.totalLiquidity + deltaLiquidity,
-                params
-            );
-        } else {
-            uint256 fees = deltaY.mulWadUp(params.swapFee);
-            deltaLiquidity = fees.mulWadUp(pool.totalLiquidity).divWadUp(
-                pool.reserveY
-            ).mulWadUp(params.wY);
-            invariant = GeometricMeanLib.tradingFunction(
-                pool.reserveX - deltaX,
-                pool.reserveY + deltaY,
-                pool.totalLiquidity + deltaLiquidity,
-                params
-            );
-        }
-
-        valid = -(EPSILON) < invariant && invariant < EPSILON;
-    }
-
     /// @inheritdoc IStrategy
     function update(
         address sender,
@@ -286,7 +129,12 @@ contract GeometricMean is IStrategy {
     }
 
     /// @inheritdoc IStrategy
-    function getPoolParams(uint256 poolId) public view returns (bytes memory) {
+    function getPoolParams(uint256 poolId)
+        public
+        view
+        override
+        returns (bytes memory)
+    {
         GeometricMeanParams memory params;
 
         params.wX = internalParams[poolId].wX.actualized();
@@ -297,15 +145,29 @@ contract GeometricMean is IStrategy {
         return abi.encode(params);
     }
 
-    /// @inheritdoc IStrategy
-    function computeSwapConstant(
-        uint256 poolId,
-        bytes memory data
-    ) external view returns (int256) {
-        (uint256 rx, uint256 ry, uint256 L) =
-            abi.decode(data, (uint256, uint256, uint256));
+    function tradingFunction(
+        uint256 reserveX,
+        uint256 reserveY,
+        uint256 totalLiquidity,
+        bytes memory params
+    ) public pure override returns (int256) {
         return GeometricMeanLib.tradingFunction(
-            rx, ry, L, abi.decode(getPoolParams(poolId), (GeometricMeanParams))
+            reserveX,
+            reserveY,
+            totalLiquidity,
+            abi.decode(params, (GeometricMeanParams))
         );
     }
+
+    function _computeDeltaXGivenDeltaL(
+        uint256 deltaL,
+        uint256 totalLiquidity,
+        uint256 reserveX
+    ) internal view virtual override returns (uint256) { }
+
+    function _computeDeltaYGivenDeltaX(
+        uint256 deltaX,
+        uint256 reserveX,
+        uint256 reserveY
+    ) internal view virtual override returns (uint256) { }
 }
