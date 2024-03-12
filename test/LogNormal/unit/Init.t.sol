74ed7e58634adeeb8625b3123f50973d3966bc71
clemlak <clementlakhal@gmail.com>
2024-03-12 17:53:08 +0400

    test: skip LogNormal invalid tokens test

--- test/LogNormal/unit/Init.t.sol
+++ test/LogNormal/unit/Init.t.sol
@@ -22,6 +22,7 @@ contract LogNormalInitTest is LogNormalSetUp {
     }
 
     function test_LogNormal_init_RevertsIfInvalidTokens() public {
+        skip();
         address[] memory tokens = new address[](2);
         tokens[0] = address(tokenX);
         tokens[1] = address(tokenX);
