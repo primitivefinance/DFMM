469eb970dd5af10610135fcad785d31d5b4b8563
clemlak <clementlakhal@gmail.com>
2024-03-13 16:47:16 +0400

    test: add DFMM transferFrom scaling test

--- test/DFMM/unit/Internal.t.sol
+++ test/DFMM/unit/Internal.t.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
-import { DFMMSetUp, DFMM } from "./SetUp.sol";
+import { DFMMSetUp, DFMM, IDFMM } from "./SetUp.sol";
+import { ERC20WithFees } from "test/utils/ERC20WithFees.sol";
 
 contract DFMMInternal is DFMM {
     constructor(address weth_) DFMM(weth_) { }
@@ -54,5 +55,23 @@ contract DFMMInternalTest is DFMMSetUp {
             token.balanceOf(address(dfmmInternal)), preDFMMBalance + amount
         );
         assertEq(token.balanceOf(address(this)), preThisBalance - amount);
+    }
+
+    function test_DFMM_transferFrom_ScalesAmount() public {
+        uint256 amount = 1_000_000 * 10 ** 18;
+        uint256 scaledDownAmount = 1_000_000 * 10 ** 6;
+        MockERC20 token = new MockERC20("", "", 6);
+        token.mint(address(this), scaledDownAmount);
+        token.approve(address(dfmmInternal), scaledDownAmount);
+        uint256 preDFMMBalance = token.balanceOf(address(dfmmInternal));
+        uint256 preThisBalance = token.balanceOf(address(this));
+        dfmmInternal.transferFrom(address(token), amount);
+        assertEq(
+            token.balanceOf(address(dfmmInternal)),
+            preDFMMBalance + scaledDownAmount
+        );
+        assertEq(
+            token.balanceOf(address(this)), preThisBalance - scaledDownAmount
+        );
     }
 }
