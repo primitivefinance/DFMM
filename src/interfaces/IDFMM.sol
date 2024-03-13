1d28fa60d379e7e5b8b8e68242bba2b32497af25
clemlak <clementlakhal@gmail.com>
2024-03-14 00:43:09 +0400

    feat: add InvalidTokenDecimals error

--- src/interfaces/IDFMM.sol
+++ src/interfaces/IDFMM.sol
@@ -58,6 +58,9 @@ interface IDFMM {
     /// @dev Thrown when a pool is being initalized with more than eight tokens.
     error InvalidMaximumTokens();
 
+    /// @dev Thrown when a token has more than 18 decimals or less than 6.
+    error InvalidTokenDecimals();
+
     /// @dev Thrown when a new call is made during a locked state.
     error Locked();
 
