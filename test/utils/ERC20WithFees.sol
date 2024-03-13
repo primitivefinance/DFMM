48b9ac22c78abca814f09b363aa28c2d36e2e5fc
clemlak <clementlakhal@gmail.com>
2024-03-13 16:46:51 +0400

    test: update ERC20WithFees

--- test/utils/ERC20WithFees.sol
+++ test/utils/ERC20WithFees.sol
@@ -4,19 +4,24 @@ pragma solidity ^0.8.13;
 import { MockERC20 } from "solmate/test/utils/mocks/MockERC20.sol";
 
 contract ERC20WithFees is MockERC20 {
+    uint256 public immutable fee;
+
     constructor(
         string memory name_,
         string memory symbol_,
-        uint8 decimals_
-    ) MockERC20(name_, symbol_, decimals_) { }
+        uint8 decimals_,
+        uint256 fee_
+    ) MockERC20(name_, symbol_, decimals_) {
+        fee = fee_;
+    }
 
     function transfer(
         address to,
         uint256 amount
     ) public override returns (bool) {
-        uint256 fee = amount / 50;
-        super.transfer(address(0), fee);
-        return super.transfer(to, amount - fee);
+        uint256 fees = amount * fee / 10_000;
+        super.transfer(address(0), fees);
+        return super.transfer(to, amount - fees);
     }
 
     function transferFrom(
@@ -24,8 +29,8 @@ contract ERC20WithFees is MockERC20 {
         address to,
         uint256 amount
     ) public override returns (bool) {
-        uint256 fee = amount / 50;
-        super.transferFrom(from, address(0), fee);
-        return super.transferFrom(from, to, amount - fee);
+        uint256 fees = amount * fee / 10_000;
+        super.transferFrom(from, address(0), fees);
+        return super.transferFrom(from, to, amount - fees);
     }
 }
