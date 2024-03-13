89f6f06c980df30f6326bb0f4f3be64024b8b0b1
clemlak <clementlakhal@gmail.com>
2024-03-13 23:14:04 +0400

    test: add test_DFMM_swap_MintsLPTokensToFeeCollector

--- test/DFMM/unit/Swap.t.sol
+++ test/DFMM/unit/Swap.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { LPToken, Pool } from "src/DFMM.sol";
 import { DFMMSetUp } from "./SetUp.sol";
 
 contract DFMMSwapTest is DFMMSetUp {
@@ -34,5 +35,18 @@ contract DFMMSwapTest is DFMMSetUp {
 
     function test_DFMM_swap_RevertsWhenDeltaYGreaterThanReserveY() public {
         skip();
+    }
+
+    function test_DFMM_swap_MintsLPTokensToFeeCollector() public initPool {
+        Pool memory pool = dfmm.pools(POOL_ID);
+        LPToken token = LPToken(pool.liquidityToken);
+
+        uint256 preBalance = token.balanceOf(address(this));
+        uint256 deltaLiquidity = 1 ether;
+        uint256 fees = deltaLiquidity * pool.controllerFee / 1 ether;
+        uint256 feesInToken =
+            fees * token.totalSupply() / (pool.totalLiquidity + deltaLiquidity);
+        dfmm.swap(POOL_ID, abi.encode(true, 0, 0, 1, 1 ether, 1 ether, 1 ether));
+        assertEq(token.balanceOf(address(this)), preBalance + feesInToken);
     }
 }
