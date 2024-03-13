de2fa1954e2c3b222403436c444aef016142a85b
clemlak <clementlakhal@gmail.com>
2024-03-13 14:50:10 +0400

    test: add test_DFMM_transferFrom_WrapsETH

--- test/DFMM/unit/Internal.t.sol
+++ test/DFMM/unit/Internal.t.sol
@@ -1,21 +1,27 @@
 /// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "src/DFMM.sol";
+import { DFMMSetUp, DFMM } from "./SetUp.sol";
 
 contract DFMMInternal is DFMM {
-    constructor() DFMM(address(0)) { }
+    constructor(address weth_) DFMM(weth_) { }
 
     function transferFrom(address token, uint256 amount) external payable {
         _transferFrom(token, amount);
     }
 }
 
-contract DFMMInternalTest is Test {
-    DFMMInternal dfmm;
+contract DFMMInternalTest is DFMMSetUp {
+    DFMMInternal dfmmInternal;
 
-    function setUp() public {
-        dfmm = new DFMMInternal();
+    function setUp() public override {
+        super.setUp();
+        dfmmInternal = new DFMMInternal(address(weth));
+    }
+
+    function test_DFMM_transferFrom_WrapsETH() public {
+        uint256 amount = 1 ether;
+        dfmmInternal.transferFrom{ value: amount }(address(0), amount);
+        assertEq(weth.balanceOf(address(dfmmInternal)), amount);
     }
 }
