b4fb0606cfa9aa388d13743386d03abf5f7fe764
clemlak <clementlakhal@gmail.com>
2024-03-14 10:44:52 +0400

    test: LogNormal update reverts if invalid tokens test

--- test/LogNormal/unit/Init.t.sol
+++ test/LogNormal/unit/Init.t.sol
@@ -23,7 +23,6 @@ contract LogNormalInitTest is LogNormalSetUp {
     }
 
     function test_LogNormal_init_RevertsIfInvalidTokens() public {
-        skip();
         address[] memory tokens = new address[](2);
         tokens[0] = address(tokenX);
         tokens[1] = address(tokenX);
