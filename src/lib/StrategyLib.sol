276ceb3f9f99dd7aec3af62c721d2d8291e14e9a
clemlak <clementlakhal@gmail.com>
2024-03-29 18:17:03 +0400

    feat: reintroduce EPSILON

--- src/lib/StrategyLib.sol
+++ src/lib/StrategyLib.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import { FixedPointMathLib } from "solmate/utils/FixedPointMathLib.sol";
 
+int256 constant EPSILON = 30;
 uint256 constant HALF = 0.5e18;
 uint256 constant ONE = 1e18;
 uint256 constant TWO = 2e18;
