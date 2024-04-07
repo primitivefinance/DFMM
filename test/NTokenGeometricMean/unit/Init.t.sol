2b52020f0091cd37912d186597a426ef084490be
clemlak <clementlakhal@gmail.com>
2024-04-07 12:30:02 +0400

    test: add NTokenG3M init test file

---
+++ test/NTokenGeometricMean/unit/Init.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import { NTokenGeometricMeanSetUp } from "./SetUp.sol";
+
+contract NTokenGeometricMeanInitTest is NTokenGeometricMeanSetUp {
+    function test_NTokenG3M_init() public { }
+}
