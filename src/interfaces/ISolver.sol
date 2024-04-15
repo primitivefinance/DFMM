c8b4e48efdaba421ae8fcc86234fd1567f0b46b0
clemlak <clementlakhal@gmail.com>
2024-04-15 14:49:10 +0400

    feat: add InvalidTokenIndex error to ISolver

--- src/interfaces/ISolver.sol
+++ src/interfaces/ISolver.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.13;
 
+error InvalidTokenIndex();
+
 interface ISolver {
     function prepareInit(
         bytes calldata params,
