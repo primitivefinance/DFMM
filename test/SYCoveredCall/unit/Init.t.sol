96b29e9ae2e7305fbf533be49db0d09e68edc674
Kinrezc <matt.czernik@gmail.com>
2024-04-19 16:49:45 -0400

    SYCC test init modifier

---
+++ test/SYCoveredCall/unit/Init.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./SetUp.sol";
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+contract SYCoveredCallInitTest is SYCoveredCallSetUp { }
