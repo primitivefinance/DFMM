5589b86579451fc37e8859646d4ccab81933ebc2
clemlak <clementlakhal@gmail.com>
2024-04-24 19:23:27 +0200

    feat: update ISolver interface

--- src/interfaces/ISolver.sol
+++ src/interfaces/ISolver.sol
@@ -3,22 +3,19 @@ pragma solidity ^0.8.13;
 
 error InvalidTokenIndex();
 
+/**
+ * @title Solver Interface
+ * @author Primitive
+ */
 interface ISolver {
-    function prepareInit(
-        bytes calldata params,
-        bytes calldata poolParams
-    ) external view returns (bytes memory);
-
     function prepareAllocation(
         uint256 poolId,
-        uint256 tokenIndex,
-        uint256 delta
+        uint256[] memory deltas
     ) external view returns (bytes memory);
 
     function prepareDeallocation(
         uint256 poolId,
-        uint256 tokenIndex,
-        uint256 delta
+        uint256 liquidityDelta
     ) external view returns (bytes memory);
 
     function prepareSwap(
@@ -28,7 +25,7 @@ interface ISolver {
         uint256 amountIn
     ) external view returns (bool, uint256, bytes memory);
 
-    function getPrice(
+    function getEstimatedPrice(
         uint256 poolId,
         uint256 tokenInIndex,
         uint256 tokenOutIndex
@@ -38,4 +35,8 @@ interface ISolver {
         external
         view
         returns (uint256[] memory reserves, uint256 totalLiquidity);
+
+    // TODO: This function might get removed because `swapFee` might be
+    // merged into default DFMM pool parameters.
+    // function getSwapFee(uint256 poolId) external view returns (uint256);
 }
