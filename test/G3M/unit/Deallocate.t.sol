f5dbe25b874836464b7c9ed251dc4b480065c73a
clemlak <clementlakhal@gmail.com>
2024-04-26 13:16:36 +0200

    test: update G3M deallocate tests with new solver

--- test/G3M/unit/Deallocate.t.sol
+++ test/G3M/unit/Deallocate.t.sol
@@ -3,19 +3,16 @@ pragma solidity ^0.8.13;
 
 import "./SetUp.sol";
 import "solmate/utils/FixedPointMathLib.sol";
-import "forge-std/console2.sol";
 
 contract G3MDeallocateTest is G3MSetUp {
     using FixedPointMathLib for uint256;
 
-    function test_G3M_deallocate_GivenX_DecreasesTotalLiquidity() public init {
-        uint256 minDeltaX = 0.1 ether;
-        bytes memory deallocateData =
-            solver.prepareDeallocation(POOL_ID, 0, minDeltaX);
-        uint256 preLiquidityBalance = liquidityOf(address(this), POOL_ID);
-
+    function test_G3M_deallocate_DecreasesTotalLiquidity() public init {
         (, uint256 preTotalLiquidity) = getReservesAndLiquidity(POOL_ID);
 
+        uint256 preLiquidityBalance = liquidityOf(address(this), POOL_ID);
+        bytes memory deallocateData =
+            solver.prepareDeallocation(POOL_ID, preLiquidityBalance / 2);
         dfmm.deallocate(POOL_ID, deallocateData);
 
         (, uint256 postTotalLiquidity) = getReservesAndLiquidity(POOL_ID);
@@ -26,33 +23,31 @@ contract G3MDeallocateTest is G3MSetUp {
         );
     }
 
-    function test_G3M_deallocate_GivenX_UpdateReserves() public init {
-        uint256 minDeltaX = 0.1 ether;
+    function test_G3M_deallocate_UpdateReserves() public init {
         (uint256[] memory preReserves,) =
             solver.getReservesAndLiquidity(POOL_ID);
 
+        uint256 preLiquidityBalance = liquidityOf(address(this), POOL_ID);
         bytes memory deallocateData =
-            solver.prepareDeallocation(POOL_ID, 0, minDeltaX);
-        dfmm.deallocate(POOL_ID, deallocateData);
-
-        (, uint256 deltaY, uint256 deltaL) =
-            abi.decode(deallocateData, (uint256, uint256, uint256));
+            solver.prepareDeallocation(POOL_ID, preLiquidityBalance / 2);
+        uint256[] memory deltas = dfmm.deallocate(POOL_ID, deallocateData);
 
         (uint256[] memory postReserves,) =
             solver.getReservesAndLiquidity(POOL_ID);
-        assertEq(preReserves[0] - minDeltaX, postReserves[0]);
-        assertEq(preReserves[1] - deltaY, postReserves[1]);
+        assertEq(preReserves[0] - deltas[0], postReserves[0]);
+        assertEq(preReserves[1] - deltas[1], postReserves[1]);
     }
 
-    function test_G3M_deallocate_GivenX_TransfersTokens() public init {
+    function test_G3M_deallocate_TransfersTokens() public init {
         uint256 minDeltaX = 0.1 ether;
         uint256 preBalanceX = tokenX.balanceOf(address(this));
         uint256 preBalanceY = tokenY.balanceOf(address(this));
         uint256 preBalanceXDFMM = tokenX.balanceOf(address(dfmm));
         uint256 preBalanceYDFMM = tokenY.balanceOf(address(dfmm));
 
+        // TODO: Use an actual amount of liquidity here
         bytes memory deallocateData =
-            solver.prepareDeallocation(POOL_ID, 0, minDeltaX);
+            solver.prepareDeallocation(POOL_ID, 0.5 ether);
         (, uint256 deltaY,) =
             abi.decode(deallocateData, (uint256, uint256, uint256));
         dfmm.deallocate(POOL_ID, deallocateData);
@@ -61,25 +56,5 @@ contract G3MDeallocateTest is G3MSetUp {
         assertEq(preBalanceY + deltaY, tokenY.balanceOf(address(this)));
         assertEq(preBalanceXDFMM - minDeltaX, tokenX.balanceOf(address(dfmm)));
         assertEq(preBalanceYDFMM - deltaY, tokenY.balanceOf(address(dfmm)));
-    }
-
-    function test_G3M_deallocate_GivenY() public init {
-        uint256 minDeltaY = 0.1 ether;
-
-        bytes memory deallocateData =
-            solver.prepareDeallocation(POOL_ID, 0, minDeltaY);
-        (uint256[] memory reserves, uint256 liquidity) =
-            getReservesAndLiquidity(POOL_ID);
-        (,, uint256 deltaLiquidity) =
-            abi.decode(deallocateData, (uint256, uint256, uint256));
-
-        (uint256[] memory deltas) = dfmm.deallocate(POOL_ID, deallocateData);
-
-        (uint256[] memory adjustedReserves, uint256 adjustedLiquidity) =
-            getReservesAndLiquidity(POOL_ID);
-
-        assertEq(adjustedReserves[0], reserves[0] - deltas[0], "bad x");
-        assertEq(adjustedReserves[1], reserves[1] - deltas[1], "bad y");
-        assertEq(adjustedLiquidity, liquidity - deltaLiquidity, "bad L");
     }
 }
